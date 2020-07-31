207f73d90b9d67a6b5acdb1dbcf2390cfc218de2
Ghanashyam Kamath <ghanashyam.kamath99@gmail.com>
2020-07-31 14:49:43 +0530

    api implemented for india

--- src/Components/Statedata.js
+++ src/Components/Statedata.js
@@ -25,16 +25,81 @@ class Statedata extends Component {
       <div className="row">
         <div className="col-md-12">
           <Accordion>
-            <Card>
-              <Card.Header>
-                <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                  Click me!
-                </Accordion.Toggle>
-              </Card.Header>
-              <Accordion.Collapse eventKey="0">
-                <Card.Body>Hello! I'm the body</Card.Body>
-              </Accordion.Collapse>
-            </Card>
+            {
+                keys.map((itm,k) => {
+
+                    let districts = this.state.stateData[itm].districtData;
+                    let district_keys = Object.keys(districts);
+
+                    let total_active = 0;
+                    let total_confirmed = 0;
+                    let total_deaths = 0;
+                    let total_recover = 0;
+                    let district_list = [];
+
+                    for(let x in districts){
+                        total_active += districts[x].active;
+                        total_confirmed += districts[x].confirmed;
+                        total_deaths += districts[x].deceased;
+                        total_recover += districts[x].recovered;
+
+                        let ob = districts[x];
+                        ob["district_name"] = x;//ob has all info including district name
+                        district_list.push(ob);
+                    }
+                    // console.log(district_list);
+                    
+
+                    
+                    return (
+                      <Card>
+                        <Card.Header>
+                          <Accordion.Toggle as={Button} eventKey={k}>
+                            {itm} -
+                            <span className="btn-dark p-1 mr-2"> Total Cases - {total_confirmed}</span>
+                            <span className="btn-dark p-1 mr-2">Active - {total_active} </span>
+                            <span className="btn-dark p-1 mr-2">Recovered - {total_recover}</span>
+                            <span className="btn-dark p-1 mr-2">Deaths - {total_deaths}</span>
+                          </Accordion.Toggle>
+                        </Card.Header>
+                        <Accordion.Collapse eventKey={k}>
+                          <Card.Body>
+                            <table className="table table-bordered table-striped">
+                              <thead>
+                                <tr>
+                                  <td>District</td>
+                                  <td>Active</td>
+                                  <td>Confirmed</td>
+                                  <td>Recovered</td>
+                                  <td>Deaths</td>
+                                </tr>
+                              </thead>
+                              <tbody>
+                                {district_list.map((itm, k) => {
+                                  return (
+                                    <tr>
+                                      <td>{itm.district_name}</td>
+                                      <td>{itm.confirmed}</td>
+                                      <td>{itm.active}</td>
+                                      <td>{itm.recovered}</td>
+                                      <td>{itm.deceased}</td>
+                                    </tr>
+                                  );
+                                })}
+                              </tbody>
+                            </table>
+                          </Card.Body>
+                        </Accordion.Collapse>
+                      </Card>
+                    );
+
+                    
+                })
+            }
+
+
+
+            
             
           </Accordion>
         </div>
